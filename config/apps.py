b5eb40909a2900197c3f63dd8a2add8a48ce0593
Seyed Mohammad Mirheydari <mohammad.mirheydari@live.com>
2025-09-01 05:36:02 +0330

    fix app.py

--- config/apps.py
+++ config/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 
 class PodcastConfig(AppConfig):
-default_auto_field = "django.db.models.BigAutoField"
-name = "podcast"
-verbose_name = "پادکست"
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "podcast"
+    verbose_name = "??????"
+
