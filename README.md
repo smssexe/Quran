f59b82225c31738e8a3c1789f7974bc787f4eab0
Seyed Mohammad Mirheydari <mohammad.mirheydari@live.com>
2025-09-07 05:46:18 +0330

    remove all python codes

--- README.md
+++
@@ -1,6 +0,0 @@
-# Podcast MVP (Django)
-
-docker run -p 8000:8000 smssexe/quran:vX.Y.Z
-
-- Panel: http://127.0.0.1:8000/admin/
-- RSS: http://127.0.0.1:8000/rss/
--- podcast/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views, feeds
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("a/<slug:slug>/", views.detail, name="detail"),
-    path("a/<slug:slug>/stream-url/", views.signed_url, name="signed_url"),
-    path("rss/", feeds.PodcastFeed(), name="rss"),
-]
